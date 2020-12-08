075f2db39b55f16de185b7ceffdcb851ad109d40
ban <taigai.randd@gamil.com>
2020-12-08 09:04:25 +0900

    Initial commit

---
+++ src/main/java/com/kohodx/insurancemathemticalservice/InsuranceMathemticalServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kohodx.insurancemathemticalservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceMathemticalServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsuranceMathemticalServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kohodx/insurancemathemticalservice/InsuranceMathemticalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kohodx.insurancemathemticalservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceMathemticalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
